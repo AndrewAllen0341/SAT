288428075a2922783e0f5d8fc9d1da4a4a1df5e2
abbeyoutain <abbeyoutain@outlook.com>
2022-02-24 14:36:46 -0600

    Completed Contact Form

--- SAT.UI.MVC/Controllers/HomeController.cs
+++ SAT.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using SAT.UI.MVC.Models;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
 
 namespace SAT.UI.MVC.Controllers
 {
@@ -25,6 +29,43 @@ namespace SAT.UI.MVC.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+            //Build the message
+            string message = $"You have received an email from {cvm.Name} with a subject of {cvm.Subject}. Please respond to {cvm.Email} with your response to the following message: <br/>{cvm.Message}";
+            //MailMessage - What sends the email
+            MailMessage mm = new MailMessage("admin@abbey-outain.com", "abbeyoutain@outlook.com", cvm.Subject, message);
+
+            //MailMessage properties
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            mm.ReplyToList.Add(cvm.Email);
+
+            //SmtpClient - Info from the host that allows this to be sent
+            SmtpClient client = new SmtpClient("mail.abbey-outain.com");
+
+            //Client Credentials
+            client.Credentials = new NetworkCredential("admin@abbey-outain.com", "P@ssw0rd");
+
+            //Try to send email
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"Your request could not be completed at this time. Please try again later. Error Message:<br/>{ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
         }
     }
 }
